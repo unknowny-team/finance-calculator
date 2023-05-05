3b818d7ad3814ef9f8add9b5a2b867abc6cb0df7
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2023-05-05 19:22:18 +0600

    Remove unnecessary comments in code

--- react-app/src/components/Answer.jsx
+++ react-app/src/components/Answer.jsx
@@ -1,6 +1,6 @@
 function Answer({body}) {
     return (
-                                            // Hide if answer is empty
+                                                 // Hide if answer is empty
         <div className="answer app-block" style={Object.keys(body).length !== 0 ? {} : {display: 'none'}}>
             <h3 className="app-block__header">Результат</h3>
             {( 
@@ -34,32 +34,3 @@ function Answer({body}) {
 }
 
 export default Answer;
-
-// {
-//     (
-//         if (body.hasOwnProperty('fullAmount')) { // Means that the ReturnOfInvestmentCalculator result is in the answer
-//             <div className="answer__body">
-//                 <p className="answer__text">Начисленный процент: {body.percentages} ₽</p>
-//                 <p className="answer__text">Сумма к концу срока: {body.fullAmount} ₽</p>
-//             </div>
-//         }
-//         else {
-//             if (Array.isArray(body.monthlyFee)) {
-//                 <div className="answer__body">
-//                         <p className="answer__text">Ежемесячные выплаты:</p>
-//                         <ol>
-//                             for (let i = 0; i < body.monthlyFee.length; i++)
-//                                 <li>{body.monthlyFee[i]}</li>
-//                         </ol>
-//                         {/* <p className="answer__text">Переплата: {body.percentages} ₽</p> */}
-//                 </div>
-//             }
-//             else {
-//                 <div className="answer__body">
-//                         <p className="answer__text">Ежемесячные выплаты: {body.monthlyFee} ₽</p>
-//                         <p className="answer__text">Переплата: {body.percentages} ₽</p>
-//                 </div>
-//             }
-//         }
-//     )
-// }
